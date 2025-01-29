bd79025c6af819c1639aa283b62b4417c5699c70
Genevieve <20102601@mail.wit.ie>
2025-01-29 10:28:17 +0000

    Mark task as done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 
 const Task = (props) => {
-  // Assign a class based on priority level
+ 
   const priorityClass = `priority ${props.priority}`;
 
   return (
-    <div className="card">
+    <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
       <p className="title">{props.title}</p>
       <p>Due: {props.deadline}</p>
       <p className="description">{props.description}</p>
       <p className={priorityClass}>Priority: {props.priority}</p>
+      <button onClick={props.markDone} className='doneButton'>Done</button>
+
+
     </div>
+
   );
 };
 
